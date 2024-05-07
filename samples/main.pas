f6e4b9f166c68f7ad78a0753cb67cae8a07a0c3a
Vinicius Sanchez <viniciuss.sanchez@gmail.com>
2024-05-07 09:15:18 -0300

    Ajustes exemplo

--- samples/main.pas
+++ samples/main.pas
@@ -26,7 +26,19 @@ uses EvoInCEP;
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-  Memo1.Text := TEvoInCEP.New.Get(Edit1.Text).CEP;
+  var LCEP := TEvoInCEP.New.Get(Edit1.Text);
+
+  Memo1.Clear;
+  Memo1.Lines.Add('CEP: ' + LCEP.CEP);
+  Memo1.Lines.Add('BAIRRO: ' + LCEP.Bairro);
+  Memo1.Lines.Add('LOGRADOURO: ' + LCEP.Logradouro);
+  Memo1.Lines.Add('CIDADE: ' + LCEP.Cidade);
+  Memo1.Lines.Add('IBGE: ' + LCEP.IBGE);
+  Memo1.Lines.Add('UF: ' + LCEP.UF);
+  Memo1.Lines.Add('COMPLEMENTO: ' + LCEP.Complemento);
+  Memo1.Lines.Add('DDD: ' + LCEP.DDD);
+  Memo1.Lines.Add('GIA: ' + LCEP.GIA);
+  Memo1.Lines.Add('SIAFI: ' + LCEP.SIAFI);
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
